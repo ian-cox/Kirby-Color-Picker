0cd3e31d1c9284f88b8232ba9926c50f5640dbad
Gerardo Cruz <gerardo.cruz@illusionseeds.com>
2017-06-14 19:40:06 -0600

    Fix color picker selector in Kirby CMS 2.4 version with the new naming convention

---
+++ field-color.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Kirby Color Picker
+ *
+ * Kirby Color Picker Field for Kirby 2.4
+ *
+ * @version   1.5.2
+ * @author    Ian Cox 
+ * @link      https://github.com/ian-cox/Kirby-Color-Picker
+ * @license   <http://opensource.org/licenses/MIT>
+ */
+
+// register the builder field
+$kirby->set('field', 'color', __DIR__ . DS . 'field');
